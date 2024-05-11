a02f2654cf326ffc3b7dd7395319f42536528214
scottsonnleitner <71006436+pringscreem@users.noreply.github.com>
2024-05-10 23:58:18 -0600

    Regex Replace

---
+++ Engine/Poo.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Poo
+{
+	public:
+
+	private:
+		int x;
+		int y;
+		int vx;
+		int vy;
+		int Width;
+		int Height;
+		bool isEaten = false;
+};//The semi-colon after classes is a common error (reminder)
