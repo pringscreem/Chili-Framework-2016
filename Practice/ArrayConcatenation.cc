356c6b2c18886c0d19cca07e81df87f861d19876
scottsonnleitner <71006436+pringscreem@users.noreply.github.com>
2024-11-07 00:08:25 -0700

    Remove comments

--- Practice/ArrayConcatenation.cc
+++ Practice/ArrayConcatenation.cc
@@ -6,13 +6,13 @@ std::vector<int> getConcatenation(std::vector<int>& nums);
 int main(int argc, char* argv[])
 {
     std::vector<int> nums = {1, 2, 3, 4, 5};
-    std::cout << "Before function\n";
-    //std::cout << "My function returns ";
+    //std::cout << "Before function\n";
+    std::cout << "My function returns: ";
     std::vector<int> ans = getConcatenation(nums);
-    std::cout << "After function\n";
+    //std::cout << "After function\n";
     for(int i = 0; i < ans.size(); i++)
     {
-        //
+        std::cout << "ans[" << i << "]: " << ans[i] << std::endl;
     }
 	return 0;
 }
@@ -20,6 +20,7 @@ int main(int argc, char* argv[])
 std::vector<int> getConcatenation(std::vector<int>& nums)
 {
     std::vector<int> ans;
+    ans.resize(2 * nums.size());
     for(int i = 0; i < nums.size(); i++)
     {
         std::cout << "First loop, i = " << i << std::endl;
