7fbaf484178b183fee69a6a3ef18ade3b04a08e1
scottsonnleitner <71006436+pringscreem@users.noreply.github.com>
2024-11-06 23:56:42 -0700

    DSA Practice

---
+++ Practice/ArrayConcatenation.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> getConcatenation(std::vector<int>& nums);
+
+int main(int argc, char* argv[])
+{
+    std::vector<int> nums = {1, 2, 3, 4, 5};
+    std::cout << "Before function\n";
+    //std::cout << "My function returns ";
+    std::vector<int> ans = getConcatenation(nums);
+    std::cout << "After function\n";
+    for(int i = 0; i < ans.size(); i++)
+    {
+        //
+    }
+	return 0;
+}
+
+std::vector<int> getConcatenation(std::vector<int>& nums)
+{
+    std::vector<int> ans;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        std::cout << "First loop, i = " << i << std::endl;
+        ans[i] = nums[i];
+    }
+    for(int i = 0; i < nums.size(); i++)
+    {
+        std::cout << "Second loop, i = " << i << std::endl;
+        ans[i + nums.size()] = nums[i];
+    }
+    return ans;
+}
